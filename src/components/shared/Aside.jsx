926dcaa4d8b3b084edbecefd7f704284811b08cd
Ilham <80796431+mhdilham-u@users.noreply.github.com>
2024-08-15 09:56:47 +0700

    Make a variable primary color and font

--- src/components/shared/Aside.jsx
+++ src/components/shared/Aside.jsx
@@ -1,28 +1,27 @@
-import { NavLink } from 'react-router-dom'
-import Title from 'antd/es/typography/Title'
-import HomeIcon from '../icons/HomeIcon'
+import { NavLink } from "react-router-dom";
+import Title from "antd/es/typography/Title";
+import logoKoi from "/logo/logo-koi.png";
+import { FileDoneOutlined, HomeOutlined } from "@ant-design/icons";
 
 const Sidebar = () => {
   return (
     <>
-      <div className='p-8'>
-        <Title level={3} style={{ color: 'black' }}>
-          Aliran Koi
-        </Title>
+      <div className="p-8 flex justify-center">
+        <img src={logoKoi} alt="Logo Koi" width={73} />
       </div>
 
-      <div className='flex flex-col gap-4 px-4'>
-        <Menu link='/dashboard'>
-          <HomeIcon /> Dashboard
+      <div className="flex flex-col gap-4 px-4">
+        <Menu link="/dashboard">
+          <HomeOutlined className="text-xl" /> Dashboard
         </Menu>
 
-        <Menu link='/orders'>
-          <HomeIcon /> Tugas
+        <Menu link="/orders">
+          <FileDoneOutlined className="text-xl" /> Orders
         </Menu>
       </div>
     </>
-  )
-}
+  );
+};
 
 const Menu = ({ link, children }) => {
   return (
@@ -30,13 +29,13 @@ const Menu = ({ link, children }) => {
       to={link}
       className={({ isActive, isPending }) =>
         `flex h-12 items-center gap-4 rounded-full pl-4 font-semibold text-gray-400 ${
-          isActive ? 'bg-blue-500 text-white' : 'text-gray-400'
+          isActive ? "bg-blue-500 text-white" : "text-gray-400"
         }`
       }
     >
       {children}
     </NavLink>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1367FF",
+      },
+      fontFamily: {
+        primary: ["Plus Jakarta Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
