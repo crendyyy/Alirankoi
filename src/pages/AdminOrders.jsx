692b285a291584422ce4ad6ce50bd57a294539fb
crendyyy <crendy.realdi12@gmail.com>
2024-08-22 01:18:43 +0700

    Can update status selected order

--- src/pages/AdminOrders.jsx
+++ src/pages/AdminOrders.jsx
@@ -3,12 +3,15 @@ import Title from "antd/es/typography/Title";
 import { useState } from "react";
 import NewTableAdminOrder from "../components/orders/NewTableAdminOrder";
 import { useDeleteOrder } from "../components/service/admin/orders/useDeleteOrder";
+import { useUpdateStatusOrder } from "../components/service/admin/orders/useUpdateStatusOrder";
 
 const AdminOrders = () => {
   const [selectedDate, setSelectedDate] = useState("");
   const [selectedStatus, setSelectedStatus] = useState("");
   const [selectedRow, setSelectedRow] = useState([]);
 
+  const updateStatusOrderMutation = useUpdateStatusOrder();
+  
   const deleteOrderMutation = useDeleteOrder();
 
   const onChangeDate = (date, dateString) => {
@@ -17,6 +20,16 @@ const AdminOrders = () => {
 
   const handleChangeStatus = (value) => {
     setSelectedStatus(value);
+  };
+
+  const handleUpdateStatusSelectedRow = (selectedRowId) => {
+    selectedRowId.forEach((orderId) => {
+      updateStatusOrderMutation.mutate({
+        id: orderId,
+        data: { status: selectedStatus },
+      });
+    });
+    window.location.reload()
   };
 
   const handleDeleteSelectedRow = (selectedRowId) => {
@@ -55,7 +68,7 @@ const AdminOrders = () => {
 
           <div className="flex flex-col w-1/2 gap-3">
             <div className="flex items-center justify-between w-full gap-2">
-              <Button type="primary" className="w-3/5 text-white bg-primary">
+              <Button type="primary" className="w-3/5 text-white bg-primary" onClick={() => handleUpdateStatusSelectedRow(selectedRow)}>
                 Update Selected Order Status
               </Button>{" "}
               |
